3bfd4998cfff39085a02b56e3eb06aa99afd0a21
cvargas22 <cesar-ed22@hotmail.com>
2016-11-07 17:26:08 -0300

    Seeders Aviso e Institucion

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Institucion;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -17,5 +17,15 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Aviso::class, function (Faker\Generator $faker){
+    $institucion = Institucion::where('rut_inst','=','81.534.189-5')->first();
+    return[
+        'titulo' => $faker->sentence(5),
+        'descripcion' => $faker->sentence(15),
+        'img' => $faker->word,
+        'rut_inst' => $institucion->rut_inst
     ];
 });
---
+++ database/seeds/AvisoTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+
+use App\Institucion;
+use App\Aviso;
+use Illuminate\Database\Seeder;
+
+
+class AvisoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('aviso')->delete();
+
+        factory(Aviso::class)->times(3)->create();
+
+        $this->command->info('Avisos creados.');
+    }
+}
