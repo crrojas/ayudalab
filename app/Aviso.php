40cd34ec7aa8723d9d43f2d0b615b49b48281c46
cvargas22 <cesar-ed22@hotmail.com>
2016-11-07 17:04:04 -0300

    Modelo institucion y Modelo aviso

---
+++ app/Aviso.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Aviso extends Model
+{
+    protected $table = 'aviso';
+
+    protected $fillable = ['descripcion', 'titulo', 'img', 'rut_inst'];
+
+    public function institucion()
+    {
+        return $this->belongsTo('Institucion');
+    }
+}
---
+++ app/Institucion.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Institucion extends Model
+{
+    protected $table = 'institucion';
+    protected $fillable = ['rut_inst',
+        'mision',
+        'vision',
+        'nombre',
+        'direccion',
+        'telefono',
+        'mail'];
+
+
+    public function aviso() {
+        return $this->hasMany('aviso');
+    }
+}
