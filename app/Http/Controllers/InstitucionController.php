1328846dbed52a847daa4184b20bbde52119d431
cvargas22 <cesar-ed22@hotmail.com>
2016-12-19 17:55:15 -0300

    Creando vista, controlador y ruta para institucion

--- app/Http/Controllers/InstitucionController.php
+++ app/Http/Controllers/InstitucionController.php
@@ -11,7 +11,7 @@ class InstitucionController extends Controller
     //
     public function index($institucion)
     {
-    	$institucion_conEspacios  = strlen("_"," ",$institucion);
+    	$institucion_conEspacios  = str_replace("_"," ",$institucion);
     	$institucion = Institucion::where('nombre',$institucion_conEspacios)->first();
     	$instituciones = Institucion::all();
     	return view('institucion',compact('institucion', 'instituciones'));  
