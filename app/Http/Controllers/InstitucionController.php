c00e8d0b16f57d5cca175931036d6b60a1c0f35c
Melombus <tvargas.vicencio@gmail.com>
2017-01-03 11:57:50 -0300

    Llevando aviso correspondiente a la vista

--- app/Http/Controllers/InstitucionController.php
+++ app/Http/Controllers/InstitucionController.php
@@ -21,9 +21,17 @@ class InstitucionController extends Controller
 
     //GET
     public function verAviso($institucion,$aviso){
+        $aux = $institucion;
         $institucion_conEspacios  = str_replace("_"," ",$institucion);
         $institucion = Institucion::where('nombre',$institucion_conEspacios)->first();
-        $instituciones = Controller::listado_instituciones();
-        return view('verAviso',compact('institucion', 'instituciones'));  
+        $aviso = Aviso::where('id',$aviso)->where('rut_inst',$institucion->rut_inst)->first();
+        if ($aviso) {
+            $instituciones = Controller::listado_instituciones();
+            return view('verAviso',compact('institucion', 'instituciones', 'aviso', 'aux'));  
+        }
+        else{
+            return view('errors/503');
+        }
+        
     }
 }
