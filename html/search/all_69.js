213acd6096ed1cbc0afeec362b79ba054f4cf18c
Travis CI <travis@travis-ci.org>
2017-04-19 03:56:07 +0000

    Deploy code docs to GitHub Pages Travis build: 52
    Commit: 2f3c9a2a5abd0650bd2082e1d50136e9fbf0d005

--- html/search/all_69.js
+++ html/search/all_69.js
@@ -1,12 +1,13 @@
 var searchData=
 [
   ['imagen',['Imagen',['../class_app_1_1_imagen.html',1,'App']]],
+  ['imagenaviso',['ImagenAviso',['../class_imagen_aviso.html',1,'']]],
   ['imagenevento',['ImagenEvento',['../class_app_1_1_imagen_evento.html',1,'App']]],
   ['imagentableseeder',['ImagenTableSeeder',['../class_imagen_table_seeder.html',1,'']]],
   ['index',['index',['../class_app_1_1_http_1_1_controllers_1_1_home_controller.html#a149eb92716c1084a935e04a8d95f7347',1,'App::Http::Controllers::HomeController\index()'],['../class_app_1_1_http_1_1_controllers_1_1_homepage_controller.html#a149eb92716c1084a935e04a8d95f7347',1,'App::Http::Controllers::HomepageController\index()'],['../class_app_1_1_http_1_1_controllers_1_1_institucion_controller.html#a621841b235a1cc220b5e4f52e458899c',1,'App::Http::Controllers::InstitucionController\index()']]],
   ['informacioninstitucional',['informacionInstitucional',['../class_app_1_1_http_1_1_controllers_1_1_home_controller.html#a2b9266e19e6e9224aaae7b4c7836ff40',1,'App::Http::Controllers::HomeController']]],
-  ['institucion',['institucion',['../class_app_1_1_aviso.html#abfb0e179daf0a8f710adbb1456e5b3eb',1,'App::Aviso\institucion()'],['../class_app_1_1_noticia.html#abfb0e179daf0a8f710adbb1456e5b3eb',1,'App::Noticia\institucion()'],['../class_app_1_1_user.html#abfb0e179daf0a8f710adbb1456e5b3eb',1,'App::User\institucion()']]],
   ['institucion',['Institucion',['../class_app_1_1_institucion.html',1,'App']]],
+  ['institucion',['institucion',['../class_app_1_1_aviso.html#abfb0e179daf0a8f710adbb1456e5b3eb',1,'App::Aviso\institucion()'],['../class_app_1_1_noticia.html#abfb0e179daf0a8f710adbb1456e5b3eb',1,'App::Noticia\institucion()'],['../class_app_1_1_user.html#abfb0e179daf0a8f710adbb1456e5b3eb',1,'App::User\institucion()']]],
   ['institucioncontroller',['InstitucionController',['../class_app_1_1_http_1_1_controllers_1_1_institucion_controller.html',1,'App::Http::Controllers']]],
   ['instituciontableseeder',['InstitucionTableSeeder',['../class_institucion_table_seeder.html',1,'']]]
 ];
--- html/search/classes_69.js
+++ html/search/classes_69.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['imagen',['Imagen',['../class_app_1_1_imagen.html',1,'App']]],
+  ['imagenaviso',['ImagenAviso',['../class_imagen_aviso.html',1,'']]],
   ['imagenevento',['ImagenEvento',['../class_app_1_1_imagen_evento.html',1,'App']]],
   ['imagentableseeder',['ImagenTableSeeder',['../class_imagen_table_seeder.html',1,'']]],
   ['institucion',['Institucion',['../class_app_1_1_institucion.html',1,'App']]],
