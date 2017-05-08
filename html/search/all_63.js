f3587df3fe64f6d33aa0851bc8f955e1d9c8e399
Travis CI <travis@travis-ci.org>
2017-05-08 12:02:03 +0000

    Deploy code docs to GitHub Pages Travis build: 66
    Commit: 31803bc2e84b19de24791d411e769e17abc35ebb

--- html/search/all_63.js
+++ html/search/all_63.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['contactcontroller',['ContactController',['../class_app_1_1_http_1_1_controllers_1_1_contact_controller.html',1,'App::Http::Controllers']]],
   ['controller',['Controller',['../class_app_1_1_http_1_1_controllers_1_1_controller.html',1,'App::Http::Controllers']]],
   ['creartablaaviso',['CrearTablaAviso',['../class_crear_tabla_aviso.html',1,'']]],
   ['creartablainstitucion',['CrearTablaInstitucion',['../class_crear_tabla_institucion.html',1,'']]],
